4d8124bbbc3cb717abf32eba36e38ed396019605
syarifmuhammad <syarifmuhammad369@gmail.com>
2023-03-12 22:43:05 +0700

    layouting section kategori layanan

--- src/components/KategoriLayanan.vue
+++ src/components/KategoriLayanan.vue
@@ -1,63 +1,41 @@
 <template>
-    <div id="panduan-pengajuan-layanan" class="px-6 mt-10">
-        <h2 class="mb-6 font-bold"><span class="text-secondary">Panduan</span> Pengajuan Layanan</h2>
-
-        <ol class="relative border-l border-gray-300 mx-2">
-            <li class="mb-6 ml-4">
-                <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
-                    1
-                </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Mahasiswa Datang ke SSC.</p>
-            </li>
-            <li class="mb-6 ml-4">
-                <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
-                    2
-                </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Mahasiswa menaruh KTM ke tempat yang
-                    telah disediakan untuk antrian.</p>
-            </li>
-            <li class="mb-6 ml-4">
-                <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
-                    3
-                </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Operator memanggil mahasiswa sesuai
-                    urutan antrian kedatangan.</p>
-            </li>
-            <li class="mb-6 ml-4">
-                <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
-                    4
-                </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Mahasiswa berkonsultasi tentang
-                    layanan ke operator.</p>
-            </li>
-            <li class="mb-6 ml-4">
-                <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
-                    5
-                </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Operator membantu mahasiswa terkait
-                    layanan SSC melalui ticketing aplikasi.</p>
-            </li>
-            <li class="mb-6 ml-4">
-                <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
-                    6
-                </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Mahasiswa akan mendapatkan informasi
-                    terkait tindak lanjut layanan berdasarkan jenis layanan (1-3 hari).</p>
-            </li>
-            <li class="mb-6 ml-4">
-                <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
-                    7
-                </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Selesai</p>
-            </li>
-        </ol>
-
-    </div>
+    <section id="kategori-layanan" class="px-6 mt-12">
+        <h1 class="mb-6 font-bold text-2xl"><span class="text-secondary">Kategori</span> Layanan</h1>
+        <div class="row">
+            <div>
+                <img src="https://picsum.photos/100" />
+                <h2>Akademik</h2>
+            </div>
+            <div>
+                <img src="https://picsum.photos/100" />
+                <h2>PuTi</h2>
+            </div>
+            <div>
+                <img src="https://picsum.photos/100" />
+                <h2>Kemahasiswaan</h2>
+            </div>
+            <div>
+                <img src="https://picsum.photos/100" />
+                <h2>Logistik</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div>
+                <img src="https://picsum.photos/100" />
+                <h2>Keuangan</h2>
+            </div>
+            <div>
+                <img src="https://picsum.photos/100" />
+                <h2>Pusat Bahasa & Perpustakaan</h2>
+            </div>
+            <div>
+                <img src="https://picsum.photos/100" />
+                <h2>FTIB</h2>
+            </div>
+            <div>
+                <img src="https://picsum.photos/100" />
+                <h2>FTEIC</h2>
+            </div>
+        </div>
+    </section>
 </template>
