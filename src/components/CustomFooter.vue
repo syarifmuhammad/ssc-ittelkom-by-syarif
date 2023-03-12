e98a176e6f631b8725b01ac61208416ea9fb6714
syarifmuhammad <syarifmuhammad369@gmail.com>
2023-03-12 22:45:30 +0700

    layout footer section

---
+++ src/components/CustomFooter.vue
@@ -0,0 +1,41 @@
+<template>
+    <footer class="w-full">
+        <section class="w-full bg-[#1E1E1E] text-white">
+            <!-- Title -->
+            <h1>Student Service Center</h1>
+            <!-- Contact -->
+            <div>
+                <h2>Contact</h2>
+                <p>Address: Jl. Ketintang No.156, Gayungan, Surabaya, Jawa Timur 60231</p>
+                <p>Phone: (+6231) 8280800</p>
+                <p>Email: humas@ittelkom-sby.ac.id</p>
+            </div>
+            <!-- About -->
+            <div>
+                <h2>Aplikasi Student Service Center</h2>
+                <p>Ditunjukkan untuk mempermudah perangkat kerja serta mahasiswa Institut Teknologi Telkom Surabaya untuk
+                    melakukan pelaporan pemintaan bantuan, demi terciptanya lingkungan kerja serta perkuliahan yang lebih
+                    baik dan efisien.</p>
+            </div>
+            <!-- Menu -->
+            <div>
+                <h2>Aplikasi Student Service Center</h2>
+                <ul>
+                    <li>
+                        <a href="">My History</a>
+                    </li>
+                    <li>
+                        <a href="">Service List</a>
+                    </li>
+                    <li>
+                        <a href="">Sign Out</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="w-full">
+            <p>&copy; 2023, IT Telkom Surabaya Student Service Center</p>
+            <p><a href="https://ittelkom-sby.ac.id/">IT Telkom Surabaya</a>. All rights reserved</p>
+        </section>
+    </footer>
+</template>
