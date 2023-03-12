2661f1766fb38278fb56eb0ba439b24a4d868404
syarifmuhammad <syarifmuhammad369@gmail.com>
2023-03-12 22:41:23 +0700

    layout and design Hero Section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,17 +1,20 @@
 <template>
-    <div class="mt-[85px]">
-        <div class="w-full h-[350px] bg-hero-mobile bg-no-repeat bg-cover bg-center">
-            <div class="w-full h-full px-6 flex items-center">
-                <div>
-                    <h1 class="text-[#222222] font-semibold"><span class="text-secondary">S</span>tudent</h1>
-                    <h1 class="text-[#222222] font-semibold"><span class="text-secondary">S</span>ervice</h1>
-                    <h1 class="text-[#222222] font-semibold"><span class="text-secondary">C</span>enter</h1>
+    <div class="mt-[85px] lg:mt-10">
+        <div class="w-full h-[350px] lg:h-auto bg-hero-mobile lg:bg-none bg-no-repeat bg-cover bg-top">
+            <div class="w-full h-full px-6 lg:px-40 flex items-center lg:justify-between">
+                <div class="lg:ml-16">
+                    <h1 class="text-[#222222] font-semibold lg:text-[46px]"><span class="text-secondary">S</span>tudent
+                    </h1>
+                    <h1 class="text-[#222222] font-semibold lg:text-[46px]"><span class="text-secondary">S</span>ervice
+                    </h1>
+                    <h1 class="text-[#222222] font-semibold lg:text-[46px]"><span class="text-secondary">C</span>enter
+                    </h1>
                     <button
                         class="bg-secondary text-white border-border-secondary rounded px-6 py-2 mt-4 text-sm font-bold">Lihat
                         Layanan</button>
                 </div>
+                <img class="hidden lg:inline max-h-[450px] rounded-2xl" src="@/assets/img/slider-6.png" alt="">
             </div>
         </div>
-        <!-- <img class="w-full absolute -z-10" src="@/assets/img/slider-6-mobile.png" alt=""> -->
     </div>
 </template>
