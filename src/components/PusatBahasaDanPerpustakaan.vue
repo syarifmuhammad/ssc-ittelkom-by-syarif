31e739d20edcb069a4bca3d28b3c2a71336fff42
syarifmuhammad <syarifmuhammad369@gmail.com>
2023-03-12 22:45:04 +0700

    layouting pusat_bahasa_perpustakaan section

---
+++ src/components/PusatBahasaDanPerpustakaan.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <div>
+            <div>
+                <div>=====</div>
+                <h1>Informasi Pusat Bahasa & Perpustakaan</h1>
+            </div>
+            <p>Merupakan unit yang bertugas mengelola administrasi perpus yang mencakup kegiatan layanan administrasi
+                perpus, pusat bahasa, dan perpustakaan. Informasi lengkap mengenai Unit Pusat Bahasa & Perpustakaan, silakan
+                kunjungi:</p>
+            <ul>
+                <li>
+                    <img src="https://picsum.photos/18" />
+                    <a href="https://www.instagram.com/akademikitts/">akademikitts</a>
+                </li>
+                <li>
+                    <img src="https://picsum.photos/18" />
+                    <a href="https://perpus.ittelkom-sby.ac.id/">https://perpus.ittelkom-sby.ac.id/</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <img src="https://picsum.photos/623/384" alt="">
+        </div>
+    </section>
+</template>
