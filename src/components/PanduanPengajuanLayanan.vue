c8a79bcc4241f7c17d9988cdb03443d29bb1247f
syarifmuhammad <syarifmuhammad369@gmail.com>
2023-03-13 15:20:43 +0700

    edit title section panduan pengajuan layanan

--- src/components/PanduanPengajuanLayanan.vue
+++ src/components/PanduanPengajuanLayanan.vue
@@ -1,6 +1,15 @@
 <template>
-    <div id="panduan-pengajuan-layanan" class="px-6 lg:px-40 mt-12">
-        <h1 class="mb-6 font-bold text-2xl"><span class="text-secondary">Panduan</span> Pengajuan Layanan</h1>
+    <div id="panduan-pengajuan-layanan" class="px-6 lg:px-40 mt-[5.625rem]">
+        <h1 class="mb-6 text-[22px]">
+            <span class="text-secondary">
+                <svg class="w-[22px] aspect-square inline-block mb-1" aria-hidden="true" focusable="false"
+                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+                    <path fill="currentColor"
+                        d="M432 64H208C199.2 64 192 71.16 192 80V96H128V80C128 35.82 163.8 0 208 0H432C476.2 0 512 35.82 512 80V304C512 348.2 476.2 384 432 384H416V320H432C440.8 320 448 312.8 448 304V80C448 71.16 440.8 64 432 64zM0 192C0 156.7 28.65 128 64 128H320C355.3 128 384 156.7 384 192V448C384 483.3 355.3 512 320 512H64C28.65 512 0 483.3 0 448V192zM96 256H288C305.7 256 320 241.7 320 224C320 206.3 305.7 192 288 192H96C78.33 192 64 206.3 64 224C64 241.7 78.33 256 96 256z">
+                    </path>
+                </svg> Panduan
+            </span> Pengajuan Layanan
+        </h1>
 
         <ol class="relative border-l border-gray-300 mx-2">
             <li class="mb-6 ml-4">
