c3e5b3db364f18a79da2924d37ee5a57b46732fc
syarifmuhammad <syarifmuhammad369@gmail.com>
2023-03-13 23:33:26 +0700

    design reusable informasi section

---
+++ src/components/kategori_layanan/Informasi.vue
@@ -0,0 +1,51 @@
+<template>
+    <section class="mt-[5.625rem] w-full">
+        <div class="w-1/2 min-h-[4rem] lg:min-h-[1.6rem] flex mb-5 lg:mb-0">
+            <div class="bg-secondary h-[1.2rem] min-w-[42px] w-[11%] rounded-[0.3rem] mr-[7%] mt-[.5%]"></div>
+            <h1 class="mb-6 lg:mb-0 text-[26px] text-secondary">
+                {{ title }}
+            </h1>
+        </div>
+        <div class="flex flex-col-reverse lg:flex-row justify-center items-center">
+            <div class="lg:w-1/2">
+                <p class="text-[#465b52] font-lato text-base font-normal">
+                    <slot></slot>
+                </p>
+                <p class="font-lato text-base font-normal mt-[55px] mb-5 flex">
+                    <svg class="h-[18px] aspect-square mt-1 mr-6 text-secondary" aria-hidden="true" focusable="false"
+                        data-prefix="fab" data-icon="instagram" role="img" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 448 512" data-fa-i2svg="">
+                        <path fill="currentColor"
+                            d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z">
+                        </path>
+                    </svg>
+                    <a :href="link_instagram.link" class="hover:text-secondary">{{ link_instagram.title }}</a>
+                </p>
+                <p class="font-lato text-base font-normal flex">
+                    <svg class="h-[18px] aspect-square mt-1 mr-6 text-secondary" aria-hidden="true" focusable="false"
+                        data-prefix="fas" data-icon="globe" role="img" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512" data-fa-i2svg="">
+                        <path fill="currentColor"
+                            d="M352 256C352 278.2 350.8 299.6 348.7 320H163.3C161.2 299.6 159.1 278.2 159.1 256C159.1 233.8 161.2 212.4 163.3 192H348.7C350.8 212.4 352 233.8 352 256zM503.9 192C509.2 212.5 512 233.9 512 256C512 278.1 509.2 299.5 503.9 320H380.8C382.9 299.4 384 277.1 384 256C384 234 382.9 212.6 380.8 192H503.9zM493.4 160H376.7C366.7 96.14 346.9 42.62 321.4 8.442C399.8 29.09 463.4 85.94 493.4 160zM344.3 160H167.7C173.8 123.6 183.2 91.38 194.7 65.35C205.2 41.74 216.9 24.61 228.2 13.81C239.4 3.178 248.7 0 256 0C263.3 0 272.6 3.178 283.8 13.81C295.1 24.61 306.8 41.74 317.3 65.35C328.8 91.38 338.2 123.6 344.3 160H344.3zM18.61 160C48.59 85.94 112.2 29.09 190.6 8.442C165.1 42.62 145.3 96.14 135.3 160H18.61zM131.2 192C129.1 212.6 127.1 234 127.1 256C127.1 277.1 129.1 299.4 131.2 320H8.065C2.8 299.5 0 278.1 0 256C0 233.9 2.8 212.5 8.065 192H131.2zM194.7 446.6C183.2 420.6 173.8 388.4 167.7 352H344.3C338.2 388.4 328.8 420.6 317.3 446.6C306.8 470.3 295.1 487.4 283.8 498.2C272.6 508.8 263.3 512 255.1 512C248.7 512 239.4 508.8 228.2 498.2C216.9 487.4 205.2 470.3 194.7 446.6H194.7zM190.6 503.6C112.2 482.9 48.59 426.1 18.61 352H135.3C145.3 415.9 165.1 469.4 190.6 503.6V503.6zM321.4 503.6C346.9 469.4 366.7 415.9 376.7 352H493.4C463.4 426.1 399.8 482.9 321.4 503.6V503.6z">
+                        </path>
+                    </svg>
+                    <a :href="link_website.link" class="hover:text-secondary">{{ link_website.title }}</a>
+                </p>
+            </div>
+            <div class="lg:w-1/2">
+                <img class="m-auto w-3/4 lg:w-full" :src="img" alt="">
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    name: "Informasi",
+    props: {
+        title: String,
+        link_instagram: Object,
+        link_website: Object,
+        img: String,
+    }
+}
+</script>
