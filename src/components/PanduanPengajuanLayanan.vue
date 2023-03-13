3c4de81865eba6faf1d2a1f2716c7f6826e930da
syarifmuhammad <syarifmuhammad369@gmail.com>
2023-03-13 23:32:40 +0700

    revision:on desktop view panduan pengajuan layanan

--- src/components/PanduanPengajuanLayanan.vue
+++ src/components/PanduanPengajuanLayanan.vue
@@ -1,9 +1,9 @@
 <template>
-    <div id="panduan-pengajuan-layanan" class="px-6 lg:px-40 mt-[5.625rem]">
+    <div id="panduan-pengajuan-layanan" class="px-6 lg:px-20 xl:px-36 mt-[5.625rem]">
         <h1 class="mb-6 text-[22px]">
             <span class="text-secondary">
-                <svg class="w-[22px] aspect-square inline-block mb-1" aria-hidden="true" focusable="false"
-                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+                <svg class="w-[22px] aspect-square" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512" data-fa-i2svg="">
                     <path fill="currentColor"
                         d="M432 64H208C199.2 64 192 71.16 192 80V96H128V80C128 35.82 163.8 0 208 0H432C476.2 0 512 35.82 512 80V304C512 348.2 476.2 384 432 384H416V320H432C440.8 320 448 312.8 448 304V80C448 71.16 440.8 64 432 64zM0 192C0 156.7 28.65 128 64 128H320C355.3 128 384 156.7 384 192V448C384 483.3 355.3 512 320 512H64C28.65 512 0 483.3 0 448V192zM96 256H288C305.7 256 320 241.7 320 224C320 206.3 305.7 192 288 192H96C78.33 192 64 206.3 64 224C64 241.7 78.33 256 96 256z">
                     </path>
@@ -11,60 +11,88 @@
             </span> Pengajuan Layanan
         </h1>
 
-        <ol class="relative border-l border-gray-300 mx-2">
-            <li class="mb-6 ml-4">
+        <ol
+            class="mt-20 border-l-[2.5px] border-secondary ml-4 xl:ml-0 xl:flex xl:border-l-0 xl:border-t-[2.5px] xl:gap-x-16">
+            <li class="relative w-full">
+                <div class="absolute hidden xl:block z-10 -left-px bg-white w-1/2 h-[4px] -top-[3px]"></div>
                 <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
+                    class="absolute w-[30px] h-[30px] z-20 bg-secondary rounded-full -top-1 -left-4 xl:-top-4 xl:left-1/2 xl:-translate-x-1/2 text-white flex items-center justify-center text-base font-lato">
                     1
                 </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Mahasiswa Datang ke SSC.</p>
+                <p
+                    class="mb-12 ml-6 text-[14.4px] text-[#4f5d77] font-lato font-light xl:text-center xl:mb-0 xl:ml-0 xl:mt-8">
+                    Mahasiswa
+                    Datang ke SSC.
+                </p>
             </li>
-            <li class="mb-6 ml-4">
+            <li class="relative w-full">
                 <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
+                    class="absolute w-[30px] h-[30px] bg-secondary rounded-full -top-1 -left-4 xl:-top-4 xl:left-1/2 xl:-translate-x-1/2 text-white flex items-center justify-center text-base font-lato">
                     2
                 </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Mahasiswa menaruh KTM ke tempat yang
+                <p
+                    class="mb-12 ml-6 text-[14.4px] text-[#4f5d77] font-lato font-light xl:text-center xl:mb-0 xl:ml-0 xl:mt-8">
+                    Mahasiswa
+                    menaruh KTM ke
+                    tempat yang
                     telah disediakan untuk antrian.</p>
             </li>
-            <li class="mb-6 ml-4">
+            <li class="relative w-full">
                 <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
+                    class="absolute w-[30px] h-[30px] bg-secondary rounded-full -top-1 -left-4 xl:-top-4 xl:left-1/2 xl:-translate-x-1/2 text-white flex items-center justify-center text-base font-lato">
                     3
                 </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Operator memanggil mahasiswa sesuai
+                <p
+                    class="mb-12 ml-6 text-[14.4px] text-[#4f5d77] font-lato font-light xl:text-center xl:mb-0 xl:ml-0 xl:mt-8">
+                    Operator
+                    memanggil
+                    mahasiswa sesuai
                     urutan antrian kedatangan.</p>
             </li>
-            <li class="mb-6 ml-4">
+            <li class="relative w-full">
                 <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
+                    class="absolute w-[30px] h-[30px] bg-secondary rounded-full -top-1 -left-4 xl:-top-4 xl:left-1/2 xl:-translate-x-1/2 text-white flex items-center justify-center text-base font-lato">
                     4
                 </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Mahasiswa berkonsultasi tentang
+                <p
+                    class="mb-12 ml-6 text-[14.4px] text-[#4f5d77] font-lato font-light xl:text-center xl:mb-0 xl:ml-0 xl:mt-8">
+                    Mahasiswa
+                    berkonsultasi
+                    tentang
                     layanan ke operator.</p>
             </li>
-            <li class="mb-6 ml-4">
+            <li class="relative w-full">
                 <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
+                    class="absolute w-[30px] h-[30px] bg-secondary rounded-full -top-1 -left-4 xl:-top-4 xl:left-1/2 xl:-translate-x-1/2 text-white flex items-center justify-center text-base font-lato">
                     5
                 </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Operator membantu mahasiswa terkait
+                <p
+                    class="mb-12 ml-6 text-[14.4px] text-[#4f5d77] font-lato font-light xl:text-center xl:mb-0 xl:ml-0 xl:mt-8">
+                    Operator
+                    membantu
+                    mahasiswa terkait
                     layanan SSC melalui ticketing aplikasi.</p>
             </li>
-            <li class="mb-6 ml-4">
+            <li class="relative w-full">
                 <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
+                    class="absolute w-[30px] h-[30px] bg-secondary rounded-full -top-1 -left-4 xl:-top-4 xl:left-1/2 xl:-translate-x-1/2 text-white flex items-center justify-center text-base font-lato">
                     6
                 </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Mahasiswa akan mendapatkan informasi
+                <p
+                    class="mb-12 ml-6 text-[14.4px] text-[#4f5d77] font-lato font-light xl:text-center xl:mb-0 xl:ml-0 xl:mt-8">
+                    Mahasiswa akan
+                    mendapatkan informasi
                     terkait tindak lanjut layanan berdasarkan jenis layanan (1-3 hari).</p>
             </li>
-            <li class="mb-6 ml-4">
+            <li class="relative w-full">
+                <div class="hidden xl:block absolute z-10 -right-px bg-white w-1/2 h-[4px] -top-[3px]"></div>
                 <div
-                    class="absolute w-6 h-6 bg-secondary rounded-full -left-3 border border-white text-white flex items-center justify-center text-[12px]">
+                    class="absolute w-[30px] z-20 h-[30px] bg-secondary rounded-full -top-1 -left-4 xl:-top-4 xl:left-1/2 xl:-translate-x-1/2 text-white flex items-center justify-center text-base font-lato">
                     7
                 </div>
-                <p class="mb-4 text-base font-normal text-gray-700">Selesai</p>
+                <p
+                    class="mb-12 ml-6 text-[14.4px] text-[#4f5d77] font-lato font-light xl:text-center xl:mb-0 xl:ml-0 xl:mt-8">
+                    Selesai</p>
             </li>
         </ol>
 
