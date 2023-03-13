39297559caef7ad2582d634b12ac2fddc0f8bb4a
syarifmuhammad <syarifmuhammad369@gmail.com>
2023-03-13 23:35:32 +0700

    show all component in master App.vue component

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
   <navbar></navbar>
   <hero></hero>
   <panduan-pengajuan-layanan></panduan-pengajuan-layanan>
+  <kategori-layanan></kategori-layanan>
+  <custom-footer></custom-footer>
 </template>
 <script>
 import Navbar from "./components/Navbar.vue"
 import Hero from "./components/Hero.vue"
 import PanduanPengajuanLayanan from "./components/PanduanPengajuanLayanan.vue"
+import KategoriLayanan from "./components/KategoriLayanan.vue"
+import CustomFooter from "./components/CustomFooter.vue"
 
 export default {
   name: "App",
@@ -14,6 +18,8 @@ export default {
     Navbar,
     Hero,
     PanduanPengajuanLayanan,
+    KategoriLayanan,
+    CustomFooter,
   }
 }
 </script>
