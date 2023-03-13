5037465da8a70eeec7970b7187555d7322fb6171
syarifmuhammad <syarifmuhammad369@gmail.com>
2023-03-13 23:31:40 +0700

    move and redesign PusatBahasaDanPerpustakaan

---
+++ src/components/kategori_layanan/PusatBahasaDanPerpustakaan.vue
@@ -0,0 +1,45 @@
+<template>
+    <!-- <section class="mt-[5.625rem]">
+        <div class="w-1/2 min-h-[4rem] flex mb-5">
+            <div class="bg-secondary h-[1.2rem] min-w-[42px] w-[11%] rounded-[0.3rem] mr-[7%] mt-[.5%]"></div>
+            <h1 class="mb-6 text-[26px] text-secondary">
+                Layanan Keuangan
+            </h1>
+        </div>
+    </section> -->
+    <section class="mt-[5.625rem]">
+        <div class="w-1/2 min-h-[4rem] flex mb-5">
+            <div class="bg-secondary h-[1.2rem] min-w-[42px] w-[11%] rounded-[0.3rem] mr-[7%] mt-[.5%]"></div>
+            <h1 class="mb-6 text-[26px] text-secondary">
+                Informasi Pusat Bahasa
+                & Perpustakaan
+            </h1>
+        </div>
+        <div>
+            <div>
+                <p>Merupakan unit yang bertugas mengelola administrasi perpus yang mencakup kegiatan layanan administrasi
+                    perpus, pusat bahasa, dan perpustakaan. Informasi lengkap mengenai Unit Pusat Bahasa & Perpustakaan,
+                    silakan
+                    kunjungi:</p>
+                <ul>
+                    <li>
+                        <img src="https://picsum.photos/18" />
+                        <a href="https://www.instagram.com/akademikitts/">akademikitts</a>
+                    </li>
+                    <li>
+                        <img src="https://picsum.photos/18" />
+                        <a href="https://perpus.ittelkom-sby.ac.id/">https://perpus.ittelkom-sby.ac.id/</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <img src="https://picsum.photos/623/384" alt="">
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    name: "PusatBahasaDanPerpustakaan",
+}
+</script>
