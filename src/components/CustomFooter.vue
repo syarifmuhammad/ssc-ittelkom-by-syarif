8d03c6a51ee5a47fce3c2368804690caf5a00e9d
syarifmuhammad <syarifmuhammad369@gmail.com>
2023-03-13 23:29:55 +0700

    design final footer

--- src/components/CustomFooter.vue
+++ src/components/CustomFooter.vue
@@ -1,41 +1,68 @@
 <template>
-    <footer class="w-full">
-        <section class="w-full bg-[#1E1E1E] text-white">
-            <!-- Title -->
-            <h1>Student Service Center</h1>
-            <!-- Contact -->
-            <div>
-                <h2>Contact</h2>
-                <p>Address: Jl. Ketintang No.156, Gayungan, Surabaya, Jawa Timur 60231</p>
-                <p>Phone: (+6231) 8280800</p>
-                <p>Email: humas@ittelkom-sby.ac.id</p>
+    <footer class="w-full mt-[5.625rem]">
+        <section class="w-full px-6 lg:px-20 xl:px-36 pt-10 pb-[45px] bg-[#1E1E1E] text-white lg:flex gap-x-[24px]">
+            <div class="lg:w-4/12">
+                <h1 class="text-2xl">Student Service Center</h1>
+                <div class="mt-5">
+                    <h4 class="text-[#90908e] text-sm mb-2.5">Contact</h4>
+                    <p class="font-lato text-white text-base mb-[5px]"><strong>Address:</strong> Jl. Ketintang No.156,
+                        Gayungan,
+                        Surabaya, Jawa
+                        Timur 60231</p>
+                    <p class="font-lato text-white text-base mb-[5px]"><strong>Phone:</strong> (+6231) 8280800</p>
+                    <p class="font-lato text-white text-base"><strong>Email:</strong> humas@ittelkom-sby.ac.id</p>
+                </div>
             </div>
             <!-- About -->
-            <div>
-                <h2>Aplikasi Student Service Center</h2>
-                <p>Ditunjukkan untuk mempermudah perangkat kerja serta mahasiswa Institut Teknologi Telkom Surabaya untuk
+            <div class="lg:w-5/12">
+                <h2 class="mt-[15px] mb-5 text-lg">Aplikasi Student Service Center</h2>
+                <p class="text-[#6c757d] font-lato italic text-base font-normal">Ditunjukkan untuk mempermudah perangkat
+                    kerja serta
+                    mahasiswa
+                    Institut
+                    Teknologi
+                    Telkom Surabaya untuk
                     melakukan pelaporan pemintaan bantuan, demi terciptanya lingkungan kerja serta perkuliahan yang lebih
                     baik dan efisien.</p>
             </div>
             <!-- Menu -->
-            <div>
-                <h2>Aplikasi Student Service Center</h2>
+            <div class="lg:w-3/12">
+                <h2 class="mt-[15px] mb-5 text-lg">My Account</h2>
                 <ul>
-                    <li>
-                        <a href="">My History</a>
+                    <li class="mb-[13px] duration-300 hover:pl-[5px]">
+                        <a class="font-lato text-sm font-normal text-white hover:text-secondary" href="">My History</a>
                     </li>
-                    <li>
-                        <a href="">Service List</a>
+                    <li class="mb-[13px] duration-300 hover:pl-[5px]">
+                        <a class="font-lato text-sm font-normal text-white hover:text-secondary" href="">Service List</a>
                     </li>
-                    <li>
-                        <a href="">Sign Out</a>
+                    <li class="duration-300 hover:pl-[5px]">
+                        <a class="font-lato text-sm font-normal text-white hover:text-secondary" href="">Sign Out</a>
                     </li>
                 </ul>
             </div>
         </section>
-        <section class="w-full">
-            <p>&copy; 2023, IT Telkom Surabaya Student Service Center</p>
-            <p><a href="https://ittelkom-sby.ac.id/">IT Telkom Surabaya</a>. All rights reserved</p>
+        <section class="w-full px-6 lg:px-20 xl:px-36 py-5 lg:flex justify-between items-center">
+            <p class="font-lato text-sm font-normal text-[#8e8e90]">&copy; 2023, <strong class="text-secondary">IT Telkom
+                    Surabaya</strong>
+                Student
+                Service
+                Center</p>
+            <p class="font-lato text-sm font-normal text-[#8e8e90]"><a class="text-secondary font-lato"
+                    href="https://ittelkom-sby.ac.id/">IT Telkom
+                    Surabaya</a>.
+                All rights
+                reserved</p>
         </section>
     </footer>
 </template>
+
+
+<style scoped>
+h1,
+h2,
+h3,
+h5,
+h6 {
+    color: #fff;
+}
+</style>
