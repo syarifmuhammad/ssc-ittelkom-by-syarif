269dd980411bf575fddb7ce279800a0b75358e52
syarifmuhammad <syarifmuhammad369@gmail.com>
2023-03-13 23:30:31 +0700

    design revision hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,19 +1,24 @@
 <template>
     <div class="mt-[85px] lg:mt-10">
         <div class="w-full h-[350px] lg:h-auto bg-hero-mobile lg:bg-none bg-no-repeat bg-cover bg-top">
-            <div class="w-full h-full px-6 lg:px-40 flex items-center lg:justify-between">
-                <div class="lg:ml-16">
-                    <h1 class="text-[#222222] font-semibold lg:text-[46px]"><span class="text-secondary">S</span>tudent
+            <div class="w-full h-full px-6 lg:px-20 xl:px-36 flex items-center lg:justify-between">
+                <div class="xl:ml-16">
+                    <h1 class="text-[#222222] font-semibold lg:text-[46px] text-[36px] font-quicksand"><span
+                            class="text-secondary">S</span>tudent
                     </h1>
-                    <h1 class="text-[#222222] font-semibold lg:text-[46px]"><span class="text-secondary">S</span>ervice
+                    <h1 class="text-[#222222] font-semibold lg:text-[46px] text-[36px] font-quicksand"><span
+                            class="text-secondary">S</span>ervice
                     </h1>
-                    <h1 class="text-[#222222] font-semibold lg:text-[46px]"><span class="text-secondary">C</span>enter
+                    <h1 class="text-[#222222] font-semibold lg:text-[46px] text-[36px] font-quicksand"><span
+                            class="text-secondary">C</span>enter
                     </h1>
+                    <p class="mt-[17px] text-[#465b52] font-lato text-[15px] font-normal">Datang. Buat Tiket. Proses.</p>
                     <button
-                        class="bg-secondary text-white border-border-secondary rounded px-6 py-2 mt-4 text-sm font-bold">Lihat
+                        class="bg-secondary text-white border-border-secondary rounded px-[30px] py-3 mt-[22px] text-sm font-bold font-lato">Lihat
                         Layanan</button>
                 </div>
-                <img class="hidden lg:inline max-h-[450px] rounded-2xl" src="@/assets/img/slider-6.png" alt="">
+                <img class="hidden lg:inline xl:max-h-[450px] lg:max-h-[350px] rounded-2xl" src="@/assets/img/slider-6.png"
+                    alt="">
             </div>
         </div>
     </div>
